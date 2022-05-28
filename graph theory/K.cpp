72723e5cd63f7b7a24b64dc3c67423bb0a012a1d
zxn64 <zxn_64@163.com>
2022-05-28 19:57:34 +0800

    Change K

--- graph theory/K.cpp
+++ graph theory/K.cpp
@@ -9,21 +9,11 @@ vector<int> to[maxn];
 int fa[maxn][20], dep[maxn], bs[maxn];
 
 void dfs(int u, int father) {
-
-    //cout<<u<<endl;
-
     fa[u][0] = father;
     dep[u] = dep[father] + 1;
-
-    //cout<<u<<' '<<0<<' '<<fa[u][0]<<endl;
-
     for (int i = 1; i < 20; i++) {
         fa[u][i] = fa[fa[u][i - 1]][i - 1];
-
-        //cout<<u<<' '<<i<<' '<<fa[u][i]<<endl;
-
     }
-
     bs[u] = 1;
     int l = to[u].size();
     for (int i = 0; i < l; i++) {
@@ -42,50 +32,19 @@ int lca(int u, int v) {
             v = fa[v][i];
         }
     }
-
-    //cout<<u<<' '<<v<<endl;
-
     if (u == v) {
-
-        //cout<<u<<endl;
-
         return u;
     }
     for (int i = 19; i >= 0 && u != v; i--) {
-        //cout<<i<<' '<<u<<' '<<v<<endl;
-        //cout<<i<<' '<<fa[u][i]<<' '<<fa[v][i]<<endl;
         if (fa[u][i] != fa[v][i]) {
             u = fa[u][i];
             v = fa[v][i];
         }
-        //cout<<i<<' '<<u<<' '<<v<<endl;
     }
-
-    //cout<<fa[u][0]<<endl;
-
     return fa[u][0];
 }
 
-/*
-15 3
-1 2
-3 4
-3 2
-8 6
-3 6
-7 6
-5 4
-13 14
-15 14
-10 9
-1 9
-11 12
-11 10
-12 13
-9 15
-8 14
-4 10
-*/
+
 int main() {
     int m, n, u, v;
     cin >> n >> m;
@@ -96,18 +55,6 @@ int main() {
         to[v].push_back(u);
     }
     dfs(1, 0);
-/*
-	for(int i=1; i<=n; i++) {
-		cout<<i<<'-';
-		for(int j=0; j<20; j++) {
-			cout<<fa[i][j]<<' ';
-		}
-		cout<<endl;
-	}
-	for(int i=1; i<=n; i++) {
-		cout<<i<<' '<<dep[i]<<endl;
-	}
-*/
     for (int i = 0; i < m; i++) {
         scanf("%d %d", &u, &v);
         cout << dep[u] + dep[v] - 2 * dep[lca(u, v)] << endl;
